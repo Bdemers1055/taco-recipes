8c13f7164281c40b87f740b1a9bb42c6d9924a6d
bdemers1055 <bdemers1055@gmail.com>
2018-07-23 19:14:03 -0400

    pull in data and display

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      taco: {},
       success: false,
       error: null,
       input: '# This is a header\n\nAnd this is a paragraph',
@@ -18,19 +19,24 @@ componentDidMount() {
 fetchRandomTacoRecipe() {
   const url = 'http://taco-randomizer.herokuapp.com/random/?full-taco=true';
   axios.get(url).then((response) => {
-    console.log('success');
+    // console.log('success', response.data);
+    this.setState({
+      taco: response.data,
+      success: true,
+    });
   })
   .catch((error) => {
     console.log('error');
   })
 };
   render() {
+    const {taco} = this.state;
     return (
       <div className="App">
         <h1>Taco of the day</h1>
         <button className="primaryBtn">TACO ME</button>
-        <h1 className="recipeTitle">Recipe Title</h1>
-        <ReactMarkdown source='# This is a header\n\nAnd this is a paragraph' />
+        <h1 className="recipeTitle">{taco.name}</h1>
+        <ReactMarkdown source={taco.recipe} />
       </div>
     );
   }
