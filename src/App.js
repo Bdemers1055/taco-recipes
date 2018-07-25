ee642d2b30ba1dda446828548da0d2f7d384b0b9
bdemers1055 <bdemers1055@gmail.com>
2018-07-24 20:58:16 -0400

    hotfix for react router gh-pages deployment

--- src/App.js
+++ src/App.js
@@ -1,161 +1,178 @@
-import React, { Component } from 'react';
-import ReactMarkdown from 'react-markdown';
-import axios from 'axios';
-import { Route } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import ReactMarkdown from "react-markdown";
+import axios from "axios";
+import { Route } from "react-router-dom";
+import "./App.css";
 
 // route: '/Condiments'
 class Condiments extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       taco: {},
       success: false,
-      error: null,
+      error: null
     };
   }
-  componentDidMount(){
+  componentDidMount() {
     this.fetchCondimentRecipe();
-  };
+  }
   fetchCondimentRecipe() {
     const { match } = this.props;
-    const condiment = `/sinker/tacofancy/master/condiments/${match.params.condiment}`;
+    const condiment = `/sinker/tacofancy/master/condiments/${
+      match.params.condiment
+    }`;
     const url = `https://raw.githubusercontent.com${condiment}`;
-    axios.get(url).then((response) => {
-      this.setState ({
-        taco: response.data,
-        success: true,
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({
+          taco: response.data,
+          success: true
+        });
+      })
+      .catch(error => {
+        console.log("error", error);
       });
-    })
-    .catch((error) => {
-        console.log('error', error);
-    })
-  };
-  render(){
-    const {taco} = this.state;
+  }
+  render() {
+    const { taco } = this.state;
     return (
       <section className="recip">
-      <ReactMarkdown source={taco} />
+        <ReactMarkdown source={taco} />
       </section>
     );
-  };
+  }
 }
 
 // route: '/Baselayers'
 class Baselayers extends Component {
-  constructor(props){
-    super(props);
-    this.state ={
-      taco: {},
-      success: false,
-      error: null,
-    };
-  }
-  componentDidMount(){
-   this.fetchBaseLayerRecipe();
-  };
-  fetchBaseLayerRecipe() {
-    const { match } = this.props;
-    const baselayer = `/sinker/tacofancy/master/base_layers/${match.params.baselayer}`;
-    const url = `https://raw.githubusercontent.com${baselayer}`;
-    axios.get(url).then((response) => {
-      this.setState({
-        taco: response.data,
-        success: true,
-      });
-      })
-      .catch((error) => {
-        console.log('error', error);
-      })
-    };
-    render(){
-      const {taco} = this.state;
-      return (
-        <section className="recipe">
-        <ReactMarkdown source={taco} />
-        </section>
-      );
-    }
-  }
-
-// route: '/Mixins'
-class Mixins extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       taco: {},
       success: false,
-      error: null,
+      error: null
     };
   }
-componentDidMount() {
-  this.fetchMixinRecipe();
-};
-fetchMixinRecipe() {
-  const { match } = this.props;
-  const mixin = `/sinker/tacofancy/master/mixins/${match.params.mixin}`;
-  const url = `https://raw.githubusercontent.com${mixin}`;
-  axios.get(url).then((response) => {
-    this.setState({
-      taco: response.data,
-      success: true,
-    });
-  })
-  .catch((error) => {
-    console.log('error', error);
-  })
-};
-render(){
-  const {taco} = this.state;
-  return (
-    <section className="recipe">
-      <ReactMarkdown source={taco} />
-    </section>
-  );
+  componentDidMount() {
+    this.fetchBaseLayerRecipe();
+  }
+  fetchBaseLayerRecipe() {
+    const { match } = this.props;
+    const baselayer = `/sinker/tacofancy/master/base_layers/${
+      match.params.baselayer
+    }`;
+    const url = `https://raw.githubusercontent.com${baselayer}`;
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({
+          taco: response.data,
+          success: true
+        });
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
+  }
+  render() {
+    const { taco } = this.state;
+    return (
+      <section className="recipe">
+        <ReactMarkdown source={taco} />
+      </section>
+    );
+  }
 }
+
+// route: '/Mixins'
+class Mixins extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      taco: {},
+      success: false,
+      error: null
+    };
+  }
+  componentDidMount() {
+    this.fetchMixinRecipe();
+  }
+  fetchMixinRecipe() {
+    const { match } = this.props;
+    const mixin = `/sinker/tacofancy/master/mixins/${match.params.mixin}`;
+    const url = `https://raw.githubusercontent.com${mixin}`;
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({
+          taco: response.data,
+          success: true
+        });
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
+  }
+  render() {
+    const { taco } = this.state;
+    return (
+      <section className="recipe">
+        <ReactMarkdown source={taco} />
+      </section>
+    );
+  }
 }
 
 // route: '/'
 class Home extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       taco: {},
       success: false,
-      error: null,
+      error: null
     };
   }
-componentDidMount() {
-  this.fetchRandomTacoRecipe();
-};
-fetchRandomTacoRecipe() {
-  const random = '/random/?full-taco=true';
-  const url = `https://taco-randomizer.herokuapp.com${random}`;
-  axios.get(url).then((response) => {
-    this.setState({
-      taco: response.data,
-      success: true,
-    });
-  })
-  .catch((error) => {
-    // console.log('error', error);
-    alert('Oh no! something went wrong.', error);
-  })
-};
-render(){
-  const {taco} = this.state;
-  return (
-    <div>
-    <header className="header">
-    <h1>Taco of the day</h1>
-    <button className="primaryBtn" onClick={this.fetchRandomTacoRecipe.bind(this)}><h1>TACO ME</h1></button>
-    </header>
-    <section className="recipe">
-      <ReactMarkdown source={taco.recipe} />
-    </section>
-    </div>
-  );
-}
+  componentDidMount() {
+    this.fetchRandomTacoRecipe();
+  }
+  fetchRandomTacoRecipe() {
+    const random = "/random/?full-taco=true";
+    const url = `https://taco-randomizer.herokuapp.com${random}`;
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({
+          taco: response.data,
+          success: true
+        });
+      })
+      .catch(error => {
+        // console.log('error', error);
+        alert("Oh no! something went wrong.", error);
+      });
+  }
+  render() {
+    const { taco } = this.state;
+    return (
+      <div>
+        <header className="header">
+          <h1>Taco of the day</h1>
+          <button
+            className="primaryBtn"
+            onClick={this.fetchRandomTacoRecipe.bind(this)}
+          >
+            <h1>TACO ME</h1>
+          </button>
+        </header>
+        <section className="recipe">
+          <ReactMarkdown source={taco.recipe} />
+        </section>
+      </div>
+    );
+  }
 }
 
 class App extends Component {
@@ -163,10 +180,19 @@ class App extends Component {
     return (
       <div className="app">
         <section className="recipe">
-        <Route exact path='/' component={Home} />
-        <Route path='/mixins/:mixin' component={Mixins} />
-        <Route path='/base_layers/:baselayer' component={Baselayers} />
-        <Route path='/condiments/:condiment' component={Condiments} />
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
+          <Route
+            path={`${process.env.PUBLIC_URL}//mixins/:mixin`}
+            component={Mixins}
+          />
+          <Route
+            path={`${process.env.PUBLIC_URL}//base_layers/:baselayer`}
+            component={Baselayers}
+          />
+          <Route
+            path={`${process.env.PUBLIC_URL}//condiments/:condiment`}
+            component={Condiments}
+          />
         </section>
       </div>
     );
